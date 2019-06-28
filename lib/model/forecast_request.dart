f558bc24d4f18d8c51341669ff0ed26fd1ac80cd
Jonathan Moallem <jdmoallem@gmail.com>
2019-06-28 21:30:13 +1000

    Separated forecast models from requests

--- lib/model/forecast_request.dart
+++ lib/model/forecast_request.dart
@@ -1,15 +1,31 @@
 class ForecastRequest {
+  final Forecast current;
+  final List<Forecast> hourly;
+
+  ForecastRequest({this.current, this.hourly});
+
+  factory ForecastRequest.fromJson(Map<String, dynamic> json) {
+    List<dynamic> hourlyJson = json['hourly']['data'];
+
+    return ForecastRequest(
+      current: Forecast.fromJson(json['currently']),
+      hourly: hourlyJson.map((i)=>Forecast.fromJson(i)).toList(),
+    );
+  }
+}
+
+class Forecast {
   final String summary;
   final double temperature;
   final double windSpeed;
 
-  ForecastRequest({this.summary, this.temperature, this.windSpeed});
+  Forecast({this.summary, this.temperature, this.windSpeed});
 
-  factory ForecastRequest.fromJson(Map<String, dynamic> json) {
-    return ForecastRequest(
-      summary: json['currently']['summary'],
-      temperature: json['currently']['temperature'].toDouble(),
-      windSpeed: json['currently']['windSpeed'],
+  factory Forecast.fromJson(Map<String, dynamic> json) {
+    return Forecast(
+      summary: json['summary'],
+      temperature: json['temperature'].toDouble(),
+      windSpeed: json['windSpeed'].toDouble(),
     );
   }
 }
